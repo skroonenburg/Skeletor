f0f8a90163b7cbbac74d1d9e70f75537da984a9e
Padgett Rowell <padgett@padgett.com.au>
2012-09-12 16:26:07 +0800

    Added NHibernate packages, setup NHibernate configuration static method

--- src/Skeletor.Web.UI/Global.asax.cs
+++ src/Skeletor.Web.UI/Global.asax.cs
@@ -2,17 +2,23 @@
 using System.Web.Mvc;
 using System.Web.Routing;
 using Skeletor.Web.UI.Infrastructure.IoC;
+using Skeletor.Web.UI.Infrastructure.Nhibernate;
+using NHibernate;
 
 namespace Skeletor.Web.UI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        private ISessionFactory sessionFactory;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            sessionFactory = NhibernateBootstrap.Configure();
             IoC.Container = IoCBootStrap.Configure();
         }
     }
---
+++ src/Skeletor.Web.UI/Infrastructure/Nhibernate/NhibernateBootstrap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Cfg;
+using NHibernate;
+using Skeletor.Core.Help;
+
+namespace Skeletor.Web.UI.Infrastructure.Nhibernate
+{
+    public static class NhibernateBootstrap
+    {
+        public static ISessionFactory Configure()
+        {
+            // load all models from the Skeletor.Core assembly that inherit from ClassMap
+
+            return Fluently.Configure()
+                .Mappings(m =>
+                    m.FluentMappings.AddFromAssemblyOf<Help>())  
+                 .BuildSessionFactory();
+        }
+
+    }
+}
